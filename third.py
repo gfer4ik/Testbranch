1e7f30800280b3d6b6e2769439a38a99c5ec8318
gfer4ik <gfer4ikf@gmail.com>
2024-02-09 20:34:52 +0300

    Schitaem e

--- third.py
+++ third.py
@@ -1,1 +1,11 @@
-print('Goodbye!')
+test_str = "qwertyqwe123"
+
+count = 0
+ 
+for i in test_str:
+    if i == 'e':
+        count = count + 1
+ 
+# printing result
+print("Count of e in qwertyqwe123 is : "
+      + str(count))
